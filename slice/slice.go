717621a08b83d2a27721c655285554cda3857ab7
shenchangqing <shenqinpeng@126.com>
2025-03-23 22:40:08 +0800

    fix: T type comparable

--- slice/slice.go
+++ slice/slice.go
@@ -1,16 +1,15 @@
 package slice
 
-import "cmp"
-
 // Depulicate removes duplicate elements from a string slice
 // use a map to store the unique elements
-func Depulicate[T cmp.Ordered](s []T) []T {
+func Depulicate[T comparable](s []T) []T {
 	r := make([]T, 0)
 	m := make(map[T]bool)
 
 	for _, v := range s {
 		if !m[v] {
 			r = append(r, v)
+			m[v] = true
 		}
 	}
 	return r
