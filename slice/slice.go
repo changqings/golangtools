e7858478cc1625d38e483da8788e994be44a45b2
shenchangqing <shenqinpeng@126.com>
2025-03-23 22:25:15 +0800

    update: slice Depulicate to T

--- slice/slice.go
+++ slice/slice.go
@@ -1,10 +1,12 @@
 package slice
 
+import "cmp"
+
 // Depulicate removes duplicate elements from a string slice
 // use a map to store the unique elements
-func Depulicate(s []string) []string {
-	r := make([]string, 0)
-	m := make(map[string]bool)
+func Depulicate[T cmp.Ordered](s []T) []T {
+	r := make([]T, 0)
+	m := make(map[T]bool)
 
 	for _, v := range s {
 		if !m[v] {
